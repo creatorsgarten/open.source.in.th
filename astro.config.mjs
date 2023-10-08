69554627dcda1a935067965ef3ebd4ddbb9a8e31
Bunyasit Fang <wachaa1319@gmail.com>
2023-10-08 16:02:22 +0700

    removed rss for build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind()]
-});
+	site: 'https://open.source.in.th',
+	integrations: [mdx(), sitemap(), tailwind()]
+})
